591d8a9f19bf833b540321e2402e2ad06c28b670
MoktiBubu <pitic38@gmail.com>
2025-03-05 16:40:15 +0200

    script.js

--- script.js
+++ script.js
@@ -1,74 +1,70 @@
-let particles = [];
+        let particles = [];
 
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    noStroke();
+        function setup() {
+            createCanvas(windowWidth, windowHeight);
+            noStroke();
 
-    let centerX = width / 2;
-    let centerY = height / 2;
-    let scaleFactor = 10;
-    
-    // Dimensiunea maximită a inimii
-    let radius = 150;
+            let centerX = width / 2;
+            let centerY = height / 2;
+            let scaleFactor = 10;
 
-    // Parcurgem o zonă destul de mare pentru a umple inima
-    for (let x = centerX - radius; x < centerX + radius; x++) {
-        for (let y = centerY - radius; y < centerY + radius; y++) {
-            // Verificăm dacă punctul (x, y) este în interiorul formei inimii
-            let t = atan2(y - centerY, x - centerX);
-            let distHeart = 16 * pow(sin(t), 3); // Forma în X
-            let yHeart = 13 * cos(t) - 5 * cos(2 * t) - 2 * cos(3 * t) - cos(4 * t); // Forma în Y
-            
-            // Verificăm dacă punctul se află în interiorul formei inimii
-            if (pow(x - centerX - distHeart, 2) + pow(y - centerY + yHeart, 2) < pow(scaleFactor, 2)) {
-                particles.push(new Particle(x, y)); // Adăugăm particula
+            for (let i = 0; i < 10000; i++) {
+                let t = random(TWO_PI);
+                let r = sqrt(random(0, 1)); 
+                let x = 16 * pow(sin(t), 3) * r;
+                let y = (13 * cos(t) - 5 * cos(2 * t) - 2 * cos(3 * t) - cos(4 * t)) * r;
+
+                x = centerX + x * scaleFactor;
+                y = centerY - y * scaleFactor;
+
+                particles.push(new Particle(x, y));
             }
         }
-    }
-}
 
-function draw() {
-    background(0);
-    for (let p of particles) {
-        p.update();
-        p.show();
-    }
-}
-
-// Clasa pentru particule
-class Particle {
-    constructor(x, y) {
-        this.origX = x;
-        this.origY = y;
-        this.x = x;
-        this.y = y;
-        this.vx = 0;
-        this.vy = 0;
-    }
-
-    update() {
-        let d = dist(mouseX, mouseY, this.x, this.y);
-        if (d < 60) {
-            let angle = atan2(this.y - mouseY, this.x - mouseX);
-            this.vx = cos(angle) * 2;
-            this.vy = sin(angle) * 2;
-        } else {
-            this.vx *= 0.9;
-            this.vy *= 0.9;
-            this.x += (this.origX - this.x) * 0.05;
-            this.y += (this.origY - this.y) * 0.05;
+        function draw() {
+            background(0);
+            for (let p of particles) {
+                p.update();
+                p.show();
+            }
         }
 
-        this.x += this.vx;
-        this.y += this.vy;
-    }
+        class Particle {
+            constructor(x, y) {
+                this.origX = x;
+                this.origY = y;
+                this.x = x + random(-1, 1);
+                this.y = y + random(-1, 1);
+                this.vx = 0;
+                this.vy = 0;
+            }
 
-    show() {
-        fill(255, 0, 0);
-        ellipse(this.x, this.y, 4);
-    }
-}
+            update() {
+                let d = dist(mouseX, mouseY, this.x, this.y);
+                if (d < 60) {
+                    let angle = atan2(this.y - mouseY, this.x - mouseX);
+                    this.vx = cos(angle) * 2;
+                    this.vy = sin(angle) * 2;
+                } else {
+                    this.vx = 0.9;
+                    this.vy = 0.9;
+                    this.x += (this.origX - this.x) * 0.05;
+                    this.y += (this.origY - this.y) * 0.05;
+                }
 
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-}
+                this.x += this.vx;
+                this.y += this.vy;
+            }
+
+            show() {
+                fill(255, 0, 0, 255);
+                ellipse(this.x, this.y, 3);
+            }
+        }
+
+        function windowResized() {
+            resizeCanvas(windowWidth, windowHeight);
+        }
+    </script>
+</body>
+</html>
