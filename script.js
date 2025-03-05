c3f3d8228f0eddf6e9a373036c1e202c8e7ca5de
MoktiBubu <pitic38@gmail.com>
2025-03-05 15:49:55 +0200

    script.js

--- script.js
+++ script.js
@@ -1,31 +1,48 @@
 let particles = [];
-let textParticles = [];
+let font;
+let textPoints = [];
 let heartSize = 10;
+let particleDensity = 0.25; // Controlează câte particule sunt create
+
+function preload() {
+    font = loadFont('https://cdnjs.cloudflare.com/ajax/libs/topcoat/0.8.0/font/SourceCodePro-Bold.otf');
+}
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     noStroke();
     
-    // Creăm particulele pentru inimă
-    for (let a = 0; a < TWO_PI; a += 0.05) {
-        let x = 16 * pow(sin(a), 3) * heartSize;
-        let y = -(13 * cos(a) - 5 * cos(2 * a) - 2 * cos(3 * a) - cos(4 * a)) * heartSize;
-        particles.push(new Particle(x, y));
-    }
-
-    // Creăm particule pentru textul "Te iubesc"
     let txt = "Te iubesc";
-    textSize(32);
-    let txtW = textWidth(txt);
-    let txtX = -txtW / 2;
-    let txtY = 10;
+    let txtSize = 100;
+    let txtBounds = font.textBounds(txt, 0, 0, txtSize);
+    let txtX = -txtBounds.w / 2;
+    let txtY = txtBounds.h / 2;
+    
+    // Creăm particule pentru text
+    textPoints = font.textToPoints(txt, txtX, txtY, txtSize, { sampleFactor: 0.2 });
 
-    for (let i = 0; i < txt.length; i++) {
-        let letter = txt[i];
-        let letterX = txtX + textWidth(txt.substring(0, i));
-        let letterY = txtY;
+    // Creăm particule pentru inimă
+    for (let x = -16 * heartSize; x < 16 * heartSize; x += heartSize * particleDensity) {
+        for (let y = -13 * heartSize; y < 13 * heartSize; y += heartSize * particleDensity) {
+            let d = dist(x, y, 0, 0);
+            let heartFormula = (pow(x / heartSize, 2) + pow(y / heartSize - sqrt(abs(x / heartSize)), 2) - 1);
 
-        textParticles.push(new TextParticle(letterX, letterY, letter));
+            if (heartFormula < 0.3) { // Verificăm dacă punctul e în inimă
+                let inText = false;
+                
+                // Verificăm dacă punctul e în zona textului
+                for (let tp of textPoints) {
+                    if (dist(tp.x, tp.y, x, y) < heartSize * 0.8) {
+                        inText = true;
+                        break;
+                    }
+                }
+
+                if (!inText) {
+                    particles.push(new Particle(x, y));
+                }
+            }
+        }
     }
 }
 
@@ -33,62 +50,47 @@ function draw() {
     background(0);
     translate(width / 2, height / 2);
     
-    // Desenăm particulele inimii
-    fill(255, 0, 0);
     for (let particle of particles) {
         particle.update();
         particle.show();
     }
-
-    // Desenăm particulele textului
-    fill(255);
-    for (let tp of textParticles) {
-        tp.show();
-    }
 }
 
-// Clasa pentru particulele inimii
+// Clasă pentru particule
 class Particle {
     constructor(x, y) {
         this.origX = x;
         this.origY = y;
         this.x = x + random(-3, 3);
         this.y = y + random(-3, 3);
-        this.vx = random(-0.5, 0.5);
-        this.vy = random(-0.5, 0.5);
+        this.vx = 0;
+        this.vy = 0;
     }
 
     update() {
         let d = dist(mouseX - width / 2, mouseY - height / 2, this.x, this.y);
         if (d < 50) {
-            this.x += random(-2, 2);
-            this.y += random(-2, 2);
+            let angle = atan2(this.y - (mouseY - height / 2), this.x - (mouseX - width / 2));
+            this.vx = cos(angle) * 2;
+            this.vy = sin(angle) * 2;
         } else {
+            this.vx *= 0.9;
+            this.vy *= 0.9;
             this.x += (this.origX - this.x) * 0.05;
             this.y += (this.origY - this.y) * 0.05;
         }
+
+        this.x += this.vx;
+        this.y += this.vy;
     }
 
     show() {
+        fill(255, 0, 0);
         ellipse(this.x, this.y, 4);
-    }
-}
-
-// Clasa pentru particulele textului
-class TextParticle {
-    constructor(x, y, letter) {
-        this.x = x;
-        this.y = y;
-        this.letter = letter;
-    }
-
-    show() {
-        textSize(32);
-        textAlign(CENTER, CENTER);
-        text(this.letter, this.x, this.y);
     }
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
+
