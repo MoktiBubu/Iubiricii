1ea337b0b44fbe0026ce2b4e5d8e43b9a2fff6e0
MoktiBubu <pitic38@gmail.com>
2025-03-05 16:05:42 +0200

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 let particles = [];
 let font;
-let textMask = [];
-let heartSize = 15;
-let particleDensity = 8; // Controlează câte particule sunt create
+let textPoints = [];
 
 function preload() {
     font = loadFont('https://cdnjs.cloudflare.com/ajax/libs/topcoat/0.8.0/font/SourceCodePro-Bold.otf');
@@ -11,51 +9,54 @@ function preload() {
 function setup() {
     createCanvas(windowWidth, windowHeight);
     noStroke();
-
-    // Generăm punctele textului "Te iubesc"
+    
     let txt = "Te iubesc";
-    let txtSize = 80;
-    let txtBounds = font.textBounds(txt, 0, 0, txtSize);
-    let txtX = -txtBounds.w / 2;
-    let txtY = txtBounds.h / 2;
+    let txtSize = 100;
+    let txtX = width / 2 - 180;
+    let txtY = height / 2 + 30;
 
-    textMask = font.textToPoints(txt, txtX, txtY, txtSize, { sampleFactor: 0.3 });
+    // Obținem punctele textului
+    textPoints = font.textToPoints(txt, txtX, txtY, txtSize, { sampleFactor: 0.2 });
 
-    // Generăm particulele pentru inimă
-    for (let x = -16 * heartSize; x < 16 * heartSize; x += particleDensity) {
-        for (let y = -13 * heartSize; y < 13 * heartSize; y += particleDensity) {
-            let heartFormula = pow(x / heartSize, 2) + pow(y / heartSize - sqrt(abs(x / heartSize)), 2) - 1;
+    // Generăm particulele inimii
+    for (let i = 0; i < 1000; i++) {
+        let angle = random(TWO_PI);
+        let radius = random(0, 200);
+        let x = width / 2 + radius * cos(angle);
+        let y = height / 2 + radius * sin(angle) * 0.8;
 
-            if (heartFormula < 0) {
-                let inText = false;
-
-                // Verificăm dacă particula e în zona textului și o eliminăm
-                for (let tp of textMask) {
-                    if (dist(tp.x, tp.y, x, y) < heartSize * 0.9) {
-                        inText = true;
-                        break;
-                    }
-                }
-
-                if (!inText) {
-                    particles.push(new Particle(x, y));
-                }
-            }
+        if (isInHeart(x, y) && !isInText(x, y)) {
+            particles.push(new Particle(x, y));
         }
     }
 }
 
 function draw() {
     background(0);
-    translate(width / 2, height / 2);
-    
-    for (let particle of particles) {
-        particle.update();
-        particle.show();
+    for (let p of particles) {
+        p.update();
+        p.show();
     }
 }
 
-// Clasă pentru particule
+// Funcția care verifică dacă un punct este în inimă
+function isInHeart(x, y) {
+    let nx = (x - width / 2) / 100;
+    let ny = (y - height / 2) / 100;
+    return (nx * nx + ny * ny - 1) ** 3 - nx * nx * ny * ny * ny < 0;
+}
+
+// Funcția care verifică dacă un punct este în zona textului
+function isInText(x, y) {
+    for (let pt of textPoints) {
+        if (dist(x, y, pt.x, pt.y) < 10) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Clasa pentru particule
 class Particle {
     constructor(x, y) {
         this.origX = x;
@@ -67,9 +68,9 @@ class Particle {
     }
 
     update() {
-        let d = dist(mouseX - width / 2, mouseY - height / 2, this.x, this.y);
+        let d = dist(mouseX, mouseY, this.x, this.y);
         if (d < 50) {
-            let angle = atan2(this.y - (mouseY - height / 2), this.x - (mouseX - width / 2));
+            let angle = atan2(this.y - mouseY, this.x - mouseX);
             this.vx = cos(angle) * 3;
             this.vy = sin(angle) * 3;
         } else {
